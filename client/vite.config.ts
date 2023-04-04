1c8a730b94b142770247272c6501b0633dc578cb
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-04-04 19:49:38 +0200

    init client

---
+++ client/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:3000",
+      },
+    },
+  },
+});
